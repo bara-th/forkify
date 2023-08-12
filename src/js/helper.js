4f5df5221348464d88776bcf76302a8292a282c6
bara-th <barath000001@gmail.com>
2023-08-12 21:05:52 +0530

    commit

--- src/js/helper.js
+++ src/js/helper.js
@@ -10,7 +10,7 @@ export const timeout = function (s) {
 
 export const getJSON = async function (url) {
   try {
-    const res = await Promise.race([fetch(url), timeout(TIME_OUT)]);
+    const res = await fetch(url);
     // const res = await fetch(url);
     const data = await res.json();
 
